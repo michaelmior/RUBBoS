5396ca026cacddf4c28eb8e2d4c30a5705b662f2
Michael Mior <mmior@uwaterloo.ca>
2013-11-07 22:46:25 -0500

    Populate columns and add to story submission

--- PHP/SubmitStory.php
+++ PHP/SubmitStory.php
@@ -17,18 +17,23 @@
           "<tr><td><b>Password</b><td><input type=text size=20 name=password>\n".
           "<tr><td><b>Story title</b><td><input type=text size=100 name=title>\n".
           "<tr><td><b>Category</b><td><SELECT name=category>\n");
-    $result = mysql_query("SELECT * FROM categories", $link) or die("ERROR: Query failed");
-    while ($row = mysql_fetch_array($result))
+
+    $categories = new phpcassa\ColumnFamily($link, "Categories");
+    try {
+        $result = $categories->get_range();
+    } catch (cassandra\NotFoundException $e) {
+        $result = array();
+    } catch (Exception $e) {
+        die("ERROR: Query failed");
+    }
+    foreach ($result as $key => $row)
     {
-      print("<OPTION value=\"".$row["id"]."\">".$row["name"]."</OPTION>\n");
+      print("<OPTION value=\"".$row["name"]."\">".$row["name"]."</OPTION>\n");
     }
     print("</SELECT></table><p><br>\n".
           "<TEXTAREA rows=\"20\" cols=\"80\" name=\"body\">Write your story here</TEXTAREA><br><p>\n".
           "<input type=submit value=\"Submit this story now!\"></center><p>\n");
 
-    mysql_free_result($result);
-    mysql_close($link);
-    
     printHTMLfooter($scriptName, $startTime);
     ?>
   </body>
