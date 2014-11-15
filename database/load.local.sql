4eceed8e4d85d03afeb5e6311d916a22243c2eae
George Lestaris <glestaris@gmail.com>
2014-11-15 18:16:21 +0200

    Revert "Adds load.sql with local infile importing"
    This reverts commit 0c207c947f91494aa3faf2df46f93b31d0c22488.

--- database/load.local.sql
+++
@@ -1,9 +0,0 @@
-use rubbos;
-load data local infile "./users.data" into table users fields terminated by "\t";
-load data local infile "./stories.data" into table stories fields terminated by "\t";
-load data local infile "./comments.data" into table comments fields terminated by "\t";
-load data local infile "./old_stories.data" into table old_stories fields terminated by "\t";
-load data local infile "./old_comments.data" into table old_comments fields terminated by "\t";
-load data local infile "./submissions.data" into table submissions fields terminated by "\t";
-load data local infile "./moderator_log.data" into table moderator_log fields terminated by "\t";
-
