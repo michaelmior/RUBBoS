3f38d1ecb22aa995d1795c7154a934d5c1a25ab9
George Lestaris <glestaris@gmail.com>
2014-11-15 18:16:22 +0200

    Revert "Adds use database statements"
    This reverts commit 380d5e73b7e7b021cbdc918e3c1285131a278954.

--- database/load.sql
+++ database/load.sql
@@ -1,9 +1,8 @@
-use rubbos;
-load data infile "./users.data" into table users fields terminated by "\t";
-load data infile "./stories.data" into table stories fields terminated by "\t";
-load data infile "./comments.data" into table comments fields terminated by "\t";
-load data infile "./old_stories.data" into table old_stories fields terminated by "\t";
-load data infile "./old_comments.data" into table old_comments fields terminated by "\t";
-load data infile "./submissions.data" into table submissions fields terminated by "\t";
-load data infile "./moderator_log.data" into table moderator_log fields terminated by "\t";
+load data infile "/home/cecchet/RUBBoS/database/users.data" into table users fields terminated by "\t";
+load data infile "/home/cecchet/RUBBoS/database/stories.data" into table stories fields terminated by "\t";
+load data infile "/home/cecchet/RUBBoS/database/comments.data" into table comments fields terminated by "\t";
+load data infile "/home/cecchet/RUBBoS/database/old_stories.data" into table old_stories fields terminated by "\t";
+load data infile "/home/cecchet/RUBBoS/database/old_comments.data" into table old_comments fields terminated by "\t";
+load data infile "/home/cecchet/RUBBoS/database/submissions.data" into table submissions fields terminated by "\t";
+load data infile "/home/cecchet/RUBBoS/database/moderator_log.data" into table moderator_log fields terminated by "\t";
 
